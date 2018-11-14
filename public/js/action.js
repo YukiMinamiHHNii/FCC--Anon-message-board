f54c838306e86764eab2e2a4a221b493df2f3499
Christopher Velazquez Zuniga <homu.homu.nii@gmail.com>
2018-11-13 18:27:54 -0600

    Adds client-side form handler

---
+++ public/js/action.js
@@ -0,0 +1,60 @@
+(() => {
+	handleSubmit();
+})();
+
+function handleSubmit() {
+	document.addEventListener("submit", e => {
+		e.preventDefault();
+		let data = new FormData(e.target);
+
+		e.target.action = e.target.classList.contains("thread")
+			? `${e.target.baseURI}api/threads/${data.get("board")}`
+			: `${e.target.baseURI}api/replies/${data.get("board")}`;
+
+		if (e.target.attributes.getNamedItem("method").value == "GET" ||
+				e.target.attributes.getNamedItem("method").value == "POST") {
+			e.target.submit();
+		} else {
+			ajaxRequest(
+				{
+					type: e.target.attributes.getNamedItem("method").value,
+					endpoint: e.target.action,
+					args: data
+				},
+				result => {
+					alert(result.status);
+				}
+			);
+		}
+	});
+}
+
+function ajaxRequest(data, result) {
+	let req = getXHRType();
+
+	req.open(data.type, data.endpoint, true);
+
+	req.addEventListener("readystatechange", e => {
+		if (req.readyState === 4) {
+			if (req.status >= 200 && req.status < 400) {
+				result(JSON.parse(req.response));
+			}
+		}
+	});
+
+	if (data.type !== "GET") {
+		req.send(data.args);
+	} else {
+		req.send();
+	}
+}
+
+function getXHRType() {
+	let request;
+	if (window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	} else if (window.ActiveXObject) {
+		request = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return request;
+}
--- src/routers/threadsRouter.js
+++ src/routers/threadsRouter.js
@@ -2,19 +2,19 @@ const express = require("express"),
 			router = express.Router();
 
 router.get("/threads/:board", (req, res) => {
-	res.json({ result: "on threads GET" });
+	res.json({ status: "on threads GET" });
 });
 
 router.post("/threads/:board", (req, res) => {
-	res.json({ result: "on threads POST" });
+	res.json({ status: "on threads POST", data: req.body });
 });
 
 router.put("/threads/:board", (req, res) => {
-	res.json({ result: "on threads PUT" });
+	res.json({ status: "on threads PUT" });
 });
 
 router.delete("/threads/:board", (req, res) => {
-	res.json({ result: "on threads DELETE" });
+	res.json({ status: "on threads DELETE" });
 });
 
 module.exports = router;
